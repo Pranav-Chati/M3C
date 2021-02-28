ddcdcabe3875748ce41c395376b119d5b8fb54d5
tarikjaber <55803936+tarikjaber@users.noreply.github.com>
2021-02-28 11:52:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,39 +8,41 @@ import math
 
 import openpyxl
 
-def objective(x, a, b):
-    return pow(a, x) + b
+
+def objective(x, c_1, c_2):
+    return pow(c_1, x) + c_2
 
 
 df = pd.read_excel(sheet_name=0, io=r"C:\Users\yusef\Downloads\TCP_2021_data_FINAL.xlsx", engine='openpyxl')
 data = df.values
 
-
 # choose the input and output variables
 
 years, average_us_peak = data[7:12, 1], data[7:12, 4]
-average_us_peak_mobile = data[7:12, 9]
+average_us_peak_mobile = data[7:12, 8]
 
 print(average_us_peak)
 print(average_us_peak_mobile)
 
-averages = []
+average_us = []
 
 for i in range(5):
     sum = average_us_peak[i] + average_us_peak_mobile[i]
     average = sum / 2
-    averages.append(average)
+    average_us.append(average)
 
-pyplot.plot(years, averages, 'o')
+for year in years:
+    year = year - 2016
+
+years = [1, 2, 3, 4, 5][::-1]
+print(f"averages:{average_us}")
+
+print(f"years: {years}")
+
+pyplot.plot(years, average_us, 'o')
 pyplot.title("Average Download Speed in The United States During 2017 - 2021 (Mbps)")
+pyplot.xlabel("Years After 2016")
 pyplot.show()
-
-for value in years:
-    print(value)
-    value = value - 2016
-    print(value)
-
-print(years)
 
 # curve fit
 popt, _ = curve_fit(objective, years, average_us_peak)
@@ -50,14 +52,13 @@ print(f"{a} {b}")
 # plot input vs output
 pyplot.scatter(years, average_us_peak)
 # define a sequence of inputs between the smallest and largest known inputs
-x_line = range(min(years), max(years), 1)
+x_line = np.linspace(min(years), max(years), 20)
 # calculate the output for the range
 y_line = objective(x_line, a, b)
 # create a line plot for the mapping function
 pyplot.plot(x_line, y_line, '--', color='red')
-pyplot.title("Mbps over time")
+pyplot.title("Mbps model")
 pyplot.show()
-
 
 # loc = r"C:\Users\yusef\Downloads\TCP_2021_data_FINAL.xlsx"
 #
